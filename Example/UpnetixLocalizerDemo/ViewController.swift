c0e484eb19312da3e02f09d48918ea00dbddc548
Nadezhda Nikolova <nadezhda.nikolova@scalefocus.com>
2020-06-24 12:40:42 +0300

    testing demo app

--- Example/UpnetixLocalizerDemo/ViewController.swift
+++ Example/UpnetixLocalizerDemo/ViewController.swift
@@ -12,27 +12,46 @@ import Flexx
 class ViewController: UIViewController {
     
     @IBOutlet private weak var firstLabel: UILabel!
-    @IBOutlet private weak var secondLabel: UILabel!
+    @IBOutlet private weak var secondLabel: UILabel! {
+        didSet {
+            secondLabel.text = Flexx.shared.getString(domain: "Common", key: "name")
+        }
+    }
     @IBOutlet private weak var thirdLabel: UILabel!
+
+    var text = ""
+    var translation = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        firstLabel.text = Flexx.shared.getString(domain: "Domain", key: "key")
-        
-        Flexx.shared.getAvailableLocales(withCompletion: { languages, error in
-            
-        })
+        secondLabel.text = Flexx.shared.getString(domain: "Common", key: "name")
     }
     
     @IBAction func reloadInputViews(_ sender: Any) {
-        firstLabel.text = Flexx.shared.getString(domain: "Domain", key: "key")
+        firstLabel.text = self.text
+        secondLabel.text = self.translation
     }
     
     @IBAction func switchLanguage(_ sender: UISwitch) {
-        let locale = sender.isOn ? "en-GB" : "bg"
-        
-        Flexx.shared.changeLocale(desiredLocale: Locale(identifier: locale)) { [weak self] in
-            self?.firstLabel.text = Flexx.shared.getString(domain: "Domain", key: "key")
-        }
+
+        self.text = ""
+        self.translation = ""
+
+        Flexx.shared.getAvailableLocales(withCompletion: { languages, error in
+            for language in languages {
+                self.text.append("\n\(language.name)")
+                //                Flexx.shared.getString(domain: "Common", key: "name")
+                Flexx.shared.changeLocale(desiredLocale: Locale(identifier: language.code)) {
+                    let tex = Flexx.shared.getString(domain: "Common", key: "name")
+                    self.translation.append("\n\(tex) for lang \(language.code)")
+                }
+            }
+        })
+
+//        let locale = sender.isOn ? "en-GB" : "bg"
+//
+//        Flexx.shared.changeLocale(desiredLocale: Locale(identifier: locale)) { [weak self] in
+//            self?.firstLabel.text = Flexx.shared.getString(domain: "Domain", key: "key")
+//        }
     }
 }
